99be25a9cec80bf03c29e11e62256d0f14232efc
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-12-01 16:13:54 +0530

    Add files via upload

---
+++ Product.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    
+   float a,p,q;
+   
+   printf("Enter value of p\n");
+     scanf("%f",&p);
+
+   printf("Enter value of q\n");
+    scanf("%f",&q);
+
+     a = p*q;
+
+   printf("The product of the given two floating point numbers is = %f \n",a);
+
+
+      return 0;
+}
